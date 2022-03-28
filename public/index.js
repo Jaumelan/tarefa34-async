7af1e20da3ec5cbbbb4e1e5f60385d971b36a64a
Jaime Landazuri <jaume.lan@gmail.com>
2022-03-28 14:32:46 -0300

    update

--- public/index.js
+++ public/index.js
@@ -1,20 +1,29 @@
 const url = "https://rickandmortyapi.com/api/character/";
 const buttonRequisition = document.getElementById("firstRequisition");
-const characterId = {is: JSON.stringify(getRandomIntInclusive(1,826))};
+let characterId = JSON.stringify(getRandomIntInclusive(1,826));
 const nameArray = [];
 const printSection = document.getElementById("answer");
 const characterImage = document.getElementById("characterImage");
 const characterName = document.querySelector("h2");
 
 async function rick() {
-    let characterData = await fetch(url + characterId.is).then(data => {return data.json()});
-    console.log(characterData.name);
-    nameArray.push(characterData.name);
-    let second = await fetch(url + "?name=" + nameArray[nameArray.length - 1]).then(data => {return data.json()});
-    console.log(second);
-    characterName.innerText = second.results[0].name;
-    characterImage.src = second.results[0].image;
-    return second
+    let characterData;
+    let second;
+    try {
+        characterData = await fetch(url + characterId).then(data => {return data.json()});
+        console.log(characterData.name);
+        nameArray.push(characterData.name);
+        second = await fetch(url + "?name=" + nameArray[nameArray.length - 1]).then(data => {return data.json()});
+        console.log(second);
+        characterName.innerText = "";
+        characterImage.src = "";
+        characterName.innerText = second.results[0].name;
+        characterImage.src = second.results[0].image;
+    }
+    finally {
+        return second
+    }
+   
 };
 
 function getRandomIntInclusive(min, max) {
